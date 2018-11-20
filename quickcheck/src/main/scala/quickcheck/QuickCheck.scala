9ab6344be8e0d93f640f8b5be6f256680585405a
Yurii Melnychuk <yu.melnychuk@gmail.com>
2018-11-20 10:39:36 +0200

    week3 / QuickCheck almost all props

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,12 +9,43 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = oneOf(
+    const(empty),
+    for {
+      k <- arbitrary[A]
+      v <- arbitrary[A]
+      h <- oneOf(const(insert(v, empty)), genHeap)
+    } yield meld(h, insert(k, h))
+  )
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  property("gen1") = forAll { (h: H) =>
+  property("gen1") = forAll { h: H =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
+  }
+
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  property("min2") = forAll { (a1: Int, a2: Int)  =>
+    val min = findMin(insert(a2, insert(a1, empty)))
+    min == a1 || min == a2
+  }
+
+  property("min3") = forAll { (h1: H, h2: H)  =>
+    val melded = meld(h1, h2)
+    isEmpty(melded) || !isEmpty(h1) && findMin(melded) == findMin(h1) || !isEmpty(h2) && findMin(melded) == findMin(h2)
+  }
+
+  property("delete") = forAll { a: Int  =>
+    isEmpty(deleteMin(insert(a, empty)))
+  }
+
+  property("sorted") = forAll { h: H =>
+    def checkMin(h: H, m: A): Boolean = isEmpty(h) || findMin(h) >= m && checkMin(deleteMin(h), findMin(h))
+    isEmpty(h) || checkMin(h, findMin(h))
   }
 
 }
